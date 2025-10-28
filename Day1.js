9ea1938a4bfe7a371d80bea9a1158276e3012128
Harshith Kumar M <harshithkumarm6@gmail.com>
2025-10-28 18:52:24 +0530

    Simple HTML-CSS Project

---
+++ Day1.js
@@ -0,0 +1,15 @@
+function fun1()
+{
+    console.log("Function with 0 parameter");
+}
+fun1();
+function fun2(1)
+{
+
+    console.log("function with 1 parameter");
+    
+}
+function fun3(2,3)
+{
+
+}
